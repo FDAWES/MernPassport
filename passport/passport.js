4a691a96af7df5413f839d5f631403761a090980
Franklin Dawes <franklindawes@FDAWESOME.local>
2018-08-21 16:02:22 -0400

    Adding some more comments and leaving the Google Strategy commented out.

--- passport/passport.js
+++ passport/passport.js
@@ -2,12 +2,17 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 
+// Grab the database models
 const db = require("../models");
 
+// Passport's serialize function. Choose what info gets stored in the session.
+// In this case we're only storing the id in the session
 passport.serializeUser(function(user, done) {
   done(null, user.id);
 });
 
+// Using the id stored in the session, any requests coming in will perform this query
+// to get the user and add the user to the request object
 passport.deserializeUser(function(id, done) {
   console.log("deserialize");
   db.User.findById(id)
@@ -15,11 +20,12 @@ passport.deserializeUser(function(id, done) {
     .catch(err => done(err));
 });
 
-/********************************Local Strategey******************************/
+/********************************Local Strategy******************************/
 passport.use(
   new LocalStrategy(
     {
-      usernameField: "email"
+      // We are submitting from the client an object with the property "email" instead of "username"
+      usernameField: "email" 
     },
     function(username, password, done) {
       db.User.findOne({ email: username })
@@ -44,30 +50,38 @@ passport.use(
   )
 );
 
-/********************************Google Strategey******************************/
+/********************************Google Strategy******************************
+ * 
+ * You must go to Google's developer console and configure a project
+ * https://developers.google.com/identity/sign-in/web/sign-in#specify_your_apps_client_id
+ * 
+ * 
+ */
+ 
+//Utilizing the dotenv package you can set your GOOGLE_CLIENT_ID and SECRET in the .env file
 const options = {
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: "/auth/google/callback"
 };
 
-passport.use(
-  new GoogleStrategy(options, function(accessToken, refreshToken, profile, cb) {
-    db.User.findOne({ googleId: profile.id })
-      .then(user => {
-        if (user) {
-          return user;
-        }
+// passport.use(
+//   new GoogleStrategy(options, function(accessToken, refreshToken, profile, cb) {
+//     db.User.findOne({ googleId: profile.id })
+//       .then(user => {
+//         if (user) {
+//           return user;
+//         }
 
-        return db.User.create({
-          googleId: profile.id,
-          displayName: profile.displayName,
-          photoUrl: profile._json.image.url
-        });
-      })
-      .then(user => cb(null, user))
-      .catch(err => cb(err));
-  })
-);
+//         return db.User.create({
+//           googleId: profile.id,
+//           displayName: profile.displayName,
+//           photoUrl: profile._json.image.url
+//         });
+//       })
+//       .then(user => cb(null, user))
+//       .catch(err => cb(err));
+//   })
+// );
 
 module.exports = passport;
--- server.js
+++ server.js
@@ -1,18 +1,30 @@
-const dotenv = require("dotenv").config();
-const express = require("express");
-const path = require("path");
+// For the .env file to hide my credentials from the world
+const dotenv = require("dotenv").config(); 
+// Express
+const express = require("express"); 
+// Path
+const path = require("path"); 
+// My passport object configure with local (and potentially ) google strategy.
 const passport = require("./passport/passport");
-const session = require("express-session");
+// Express Session 
+const session = require("express-session"); 
+// Used to store our session info insid of MongoDB
 const MongoStore = require('connect-mongo')(session);
+// Mongoose
 const mongoose = require("mongoose");
+// Body Parser
 const bodyParser = require("body-parser");
+//Morgan Logger
 const logger = require("morgan")("tiny");
 
+// Pulling in my controllers to handle routing
 const routes = require("./controllers");
 
 const PORT = process.env.PORT || 3001;
+
 const app = express();
 
+// Connection to MongoDB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/my-first-mern");
 
 //#region MIDDLEWARE
@@ -21,20 +33,24 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/my-first-mern")
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-//passport
+//Setuup express to use sessions and also to save session information inside of Mongo
 app.use(session({ 
   secret: "I 4m 4bs0lut3ly aw3s0m3!", 
   resave: true, 
   saveUninitialized: true,
-  store: new MongoStore({ mongooseConnection: mongoose.connection}) 
+  store: new MongoStore({ 
+    mongooseConnection: mongoose.connection,
+    ttl: 8 * 60 * 60 // 8 Hours
+  }) 
 }));
 
+// Initialize the passport middleware and session.
 app.use(passport.initialize());
 app.use(passport.session());
-
+// Telling express to use Morgan
 app.use(logger);
 
-//controllers
+// Informing express about my routes
 app.use(routes);
 
 //#endregion MIDDLEWARE
